fa93c6551826b42032381e10ea0e6a9dd1943bbc
balajim7-a11y <balajim7@gmail.com>
2026-08-15 22:50:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,17 @@ st.set_page_config(
     page_title="Tasty India - Clubhouse Canteen",
     page_icon="🍛",
     layout="wide",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="collapsed",
 )
 
-# Read URL query parameter: ?view=admin or ?view=order
-view = st.query_params.get("view", "order").lower()
+# Read ?view=admin or ?view=order from the browser URL
+current_view = st.query_params.get("view", "order").lower()
 
-if view == "admin":
-    import views.admin as admin_view
-    admin_view.render()
+if current_view == "admin":
+  from views import admin
+
+  admin.render()
 else:
-    import views.order as order_view
-    order_view.render()
+  from views import order
+
+  order.render()
